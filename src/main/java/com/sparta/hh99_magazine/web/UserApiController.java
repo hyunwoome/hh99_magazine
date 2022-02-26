44ab8df15521c26b2f3c2072684a1ecbc51d24e1
Lim Hyunwoo <hyunwoome@icloud.com>
2022-02-26 11:55:33 +0900

    [FEAT] H2에서 MySql로 변경

--- src/main/java/com/sparta/hh99_magazine/web/UserApiController.java
+++ src/main/java/com/sparta/hh99_magazine/web/UserApiController.java
@@ -1,12 +1,15 @@
 package com.sparta.hh99_magazine.web;
 
 import com.sparta.hh99_magazine.advice.exception.LoginCheckException;
+import com.sparta.hh99_magazine.advice.exception.LoginDuplicateException;
 import com.sparta.hh99_magazine.response.MessageResponse;
+import com.sparta.hh99_magazine.security.UserDetailsImpl;
 import com.sparta.hh99_magazine.service.UserService;
 import com.sparta.hh99_magazine.web.dto.SignupRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
@@ -15,8 +18,19 @@ import org.springframework.web.bind.annotation.*;
 public class UserApiController {
     private final UserService userService;
 
-    // 1. 로그인 후 접근 X
-    // 2. 회원가입 후 접근 X
+    // 로그인 후 로그인 접근 X
+    @GetMapping("/signin")
+    public ResponseEntity<MessageResponse> signinPage(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        if (userDetails != null) { throw  new LoginDuplicateException();}
+        return new ResponseEntity<>(new MessageResponse("로그인 페이지 접근"), HttpStatus.OK);
+    }
+
+    // 로그인 후 회원가입 접근 X
+    @GetMapping("/signup")
+    public ResponseEntity<MessageResponse> signupPage(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        if (userDetails != null) { throw  new LoginDuplicateException();}
+        return new ResponseEntity<>(new MessageResponse("회원가입 페이지 접근"), HttpStatus.OK);
+    }
 
     // 회원가입
     @PostMapping("/signup")

e1e9dda344d1013708bff10086fb5b836ba65f33
Lim Hyunwoo <hyunwoome@icloud.com>
2022-02-26 15:22:23 +0900

    [FEAT] JWT Provider, Filter, WebSecurity 설정

--- src/main/java/com/sparta/hh99_magazine/security/WebSecurityConfig.java
+++ src/main/java/com/sparta/hh99_magazine/security/WebSecurityConfig.java
@@ -1,5 +1,65 @@
 package com.sparta.hh99_magazine.security;
 
+import com.sparta.hh99_magazine.jwt.JwtAuthenticationFilter;
+import com.sparta.hh99_magazine.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/*/signin", "/*/signup").permitAll()
+                .antMatchers(HttpMethod.GET, "helloworld/**").permitAll()
+                .anyRequest().hasRole("USER")
+                .and()
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    // Swagger 무시
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/v2/api-docs",
+                "/swagger-resources/**",
+                "/swagger-ui.html",
+                "webjars/**",
+                "/swagger/**");
+    }
+
+
+}
+
+
+
+
+
+/*
+// Seesion
+package com.sparta.hh99_magazine.security;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -32,7 +92,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .anyRequest()
                     .authenticated()
                     .and()
-                // TODO: 로그인이 계속 진행되는 경우 (계속 세션 발급됨)
                 .formLogin()
                     .loginPage("/api/signin")
                     .loginProcessingUrl("/api/signin")
@@ -47,3 +106,4 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .permitAll();
     }
 }
+*/

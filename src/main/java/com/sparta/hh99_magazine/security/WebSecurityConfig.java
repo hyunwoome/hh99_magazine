1eb490d2cddcdb84a4609ae91bf6d02d753b4593
Lim Hyunwoo <hyunwoome@icloud.com>
2022-02-26 16:31:44 +0900

    [ROLLBACK] JWT 설정 제거

--- src/main/java/com/sparta/hh99_magazine/security/WebSecurityConfig.java
+++ src/main/java/com/sparta/hh99_magazine/security/WebSecurityConfig.java
@@ -1,65 +1,5 @@
 package com.sparta.hh99_magazine.security;
 
-import com.sparta.hh99_magazine.jwt.JwtAuthenticationFilter;
-import com.sparta.hh99_magazine.jwt.JwtTokenProvider;
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-@RequiredArgsConstructor
-@Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    private final JwtTokenProvider jwtTokenProvider;
-
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .httpBasic().disable()
-                .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authorizeRequests()
-                .antMatchers("/*/signin", "/*/signup").permitAll()
-                .antMatchers(HttpMethod.GET, "helloworld/**").permitAll()
-                .anyRequest().hasRole("USER")
-                .and()
-                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
-    }
-
-    // Swagger 무시
-    @Override
-    public void configure(WebSecurity web) {
-        web.ignoring().antMatchers("/v2/api-docs",
-                "/swagger-resources/**",
-                "/swagger-ui.html",
-                "webjars/**",
-                "/swagger/**");
-    }
-
-
-}
-
-
-
-
-
-/*
-// Seesion
-package com.sparta.hh99_magazine.security;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -87,23 +27,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.authorizeRequests()
-                    .antMatchers("/api/**")
-                    .permitAll()
-                    .anyRequest()
-                    .authenticated()
-                    .and()
+                .antMatchers("/api/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
                 .formLogin()
-                    .loginPage("/api/signin")
-                    .loginProcessingUrl("/api/signin")
-                    .defaultSuccessUrl("/")
-                    .failureUrl("/api/signinError")
-                    .permitAll()
-                    .and()
+                .loginPage("/api/signin")
+                .loginProcessingUrl("/api/signin")
+                .defaultSuccessUrl("/")
+                .failureUrl("/api/signinError")
+                .permitAll()
+                .and()
                 .logout()
-                    .logoutSuccessUrl("/api/signoutSuccess")
-                    .invalidateHttpSession(true) // 선택
-                    .deleteCookies("JSESSIONID") // 선택
-                    .permitAll();
+                .logoutSuccessUrl("/api/signoutSuccess")
+                .invalidateHttpSession(true) // 선택
+                .deleteCookies("JSESSIONID") // 선택
+                .permitAll();
     }
 }
-*/

26075957ffd37514b6f976205b2614d24eba8fd4
Lim Hyunwoo <hyunwoome@icloud.com>
2022-02-28 10:48:34 +0900

    [FEAT] JWT 설치, SecurityConfig 설정

--- src/main/java/com/sparta/hh99_magazine/security/WebSecurityConfig.java
+++ src/main/java/com/sparta/hh99_magazine/security/WebSecurityConfig.java
@@ -1,48 +1,67 @@
 package com.sparta.hh99_magazine.security;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
-@Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Bean
-    public BCryptPasswordEncoder encodePassword() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Override
-    public void configure(WebSecurity web) {
-        web
-                .ignoring()
-                .antMatchers("/h2-console/**");
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.authorizeRequests()
-                .antMatchers("/api/**")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
+        http.httpBasic()
                 .and()
-                .formLogin()
-                .loginPage("/api/signin")
-                .loginProcessingUrl("/api/signin")
-                .defaultSuccessUrl("/")
-                .failureUrl("/api/signinError")
-                .permitAll()
-                .and()
-                .logout()
-                .logoutSuccessUrl("/api/signoutSuccess")
-                .invalidateHttpSession(true) // 선택
-                .deleteCookies("JSESSIONID") // 선택
-                .permitAll();
+                .authorizeRequests()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/user/**").hasRole("USER")
+                .antMatchers("/**").permitAll();
     }
 }
+
+// 세션
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.builders.WebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//
+//@EnableWebSecurity
+//@Configuration
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//    @Bean
+//    public BCryptPasswordEncoder encodePassword() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Override
+//    public void configure(WebSecurity web) {
+//        web
+//                .ignoring()
+//                .antMatchers("/h2-console/**");
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.csrf().disable();
+//        http.authorizeRequests()
+//                .antMatchers("/api/**")
+//                .permitAll()
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//                .formLogin()
+//                .loginPage("/api/signin")
+//                .loginProcessingUrl("/api/signin")
+//                .defaultSuccessUrl("/")
+//                .failureUrl("/api/signinError")
+//                .permitAll()
+//                .and()
+//                .logout()
+//                .logoutSuccessUrl("/api/signoutSuccess")
+//                .invalidateHttpSession(true) // 선택
+//                .deleteCookies("JSESSIONID") // 선택
+//                .permitAll();
+//    }
+//}
